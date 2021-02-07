adde30a572818f57644b3f4d22b7d56929067baa
Thiago Nogueira <thiagotn@gmail.com>
2021-02-07 12:20:33 -0300

    first commit

---
+++ lib/widgets/success.widget.dart
@@ -0,0 +1,47 @@
+import 'package:aog/widgets/loading-button.widget.dart';
+import 'package:flutter/material.dart';
+
+class Success extends StatelessWidget {
+  var result = "";
+  Function reset;
+
+  Success({
+    @required this.result,
+    @required this.reset,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(30),
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.8),
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            result,
+            style: TextStyle(
+              color: Theme.of(context).primaryColor,
+              fontSize: 40,
+              fontFamily: "Big Shoulders Display",
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          LoadingButton(
+            busy: false,
+            func: reset,
+            invert: true,
+            text: "CALCULAR NOVAMENTE",
+          )
+        ],
+      ),
+    );
+  }
+}
